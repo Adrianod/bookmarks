c624dce77b4c6de00dba267ead2c7aafe10c791f
Emil Nilsson <emil@sharpfin.com>
2023-10-18 19:32:32 +0200

    fix

--- app/dashboard/loading.tsx
+++ app/dashboard/loading.tsx
@@ -1,13 +1,11 @@
-import { Card, CardContent } from "@/components/ui/card";
+import { Card } from "@/components/ui/card";
 import { Loader } from "lucide-react";
 
 
-export default function () {
+export default function Loading() {
     return (
         <Card className="flex max-w-3xl mx-auto items-center justify-center">
-
-                <Loader className="animate-spin my-40" />
-
+            <Loader className="animate-spin my-40" />
         </Card>
     )
 }
